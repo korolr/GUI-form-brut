87a08b3fff6765dc22ba5ad57966169816d8c2b9
Aleks Da <cyberfunk9@gmail.com>
2017-01-13 21:19:43 +0300

    GtkEnty

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Handler:
+    def __init__(self):
+    	pass
+
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
     def user_file(self, button):
-    	text_a =  target_in.get_text()
+    	
+    	Main()
+
+    def target_in(self, widget, event):
+    	text_a =  self.get_text()
     	print(text_a)
     	# Main()
 
@@ -18,5 +25,39 @@ builder.connect_signals(Handler())
 
 window = builder.get_object("window1")
 window.show_all()
+target = builder.get_object("target_in").get_text()
+
+xPathLogin = builder.get_object("login_in_1").get_text()
+
+xPathPassword = builder.get_object("pass_in").get_text()
+
+xPathAcceptButton = builder.get_object("login_in").get_text()
+
+xPathSuccessAuth = builder.get_object("auth_in").get_text()
+
+xPathFailAuth = builder.get_object("login_in").get_text()
+
+selBrowserString = '*chrome'
+
+selFFProfile = 'ff_profile'
+
+
+usersFile = 'dict/users.txt'
+
+passwordsFile = 'dict/pwd.txt'
+
+resultFile = 'result.txt'
+
+brutThreads = 1
+
+rumpUpPeriod = brutThreads * 5
+
+timeout = 1
+
+randomCredentials = False
+
+
+randomGeneratorParameter = [100, 8, 1, 1, 1, 0, 0, 0]
+
 
 Gtk.main()
